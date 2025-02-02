17c5a1ddb2be9647d0576195da7819ca5fc2d74c
BlinzTheCoder <yakshgoel090@gmail.com>
2025-02-02 16:45:46 +0530

    Just a test version

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,18 +1,35 @@
 <script>
-	import '../app.css';
-	let { children } = $props();
+    import '../app.css';
+    import { page } from '$app/state';
+    let { children } = $props();
+    let navBarItems = [
+        { name: 'Home', id: '/' },
+        { name: 'Projects', id: '/Projects' },
+        { name: 'Links', id: '/Links' },
+        { name: 'Contact Me', id: '/Contact' },
+        { name: 'Posts', id: '/Posts' }
+    ];
 </script>
 
-{@render children()}
 <nav class="bg-white shadow-md">
     <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
         <div class="flex h-16 items-center justify-center">
             <div class="hidden space-x-4 sm:flex">
-				<a href="#" class="rounded-md px-3 py-2 text-sm font-medium hover:bg-gray-500 hover:text-white active:bg-gray-900 active:text-white">Home</a>
-				<a href="#" class="rounded-md px-3 py-2 text-sm font-medium hover:bg-gray-500 hover:text-white active:bg-gray-900 active:text-white">Projects</a>
-				<a href="#" class="rounded-md px-3 py-2 text-sm font-medium hover:bg-gray-500 hover:text-white active:bg-gray-900 active:text-white">Contact Me</a>
-				<a href="#" class="rounded-md px-3 py-2 text-sm font-medium hover:bg-gray-500 hover:text-white active:bg-gray-900 active:text-white">Contact Me</a>
-			</div>
+                {#each navBarItems as item}
+                    {@const path = page.url.pathname.split('/')[1]}
+                    {@const selected = path === item.id.split('/')[1]}
+
+                    <a
+                        class:bg-gray-900={selected}
+                        class:text-white={selected}
+                        href={item.id}
+                        class="rounded-md px-3 py-2 text-md font-medium transition duration-300 ease-in-out hover:bg-gray-500 hover:text-white active:bg-gray-900 active:text-white"
+                        >{item.name}</a
+                    >
+                {/each}
+            </div>
         </div>
     </div>
 </nav>
+
+{@render children()}
---
+++ src/routes/+page.svelte
@@ -0,0 +1,34 @@
+<script>
+
+</script>
+
+<div class="header text-6xl font-semibold flex justify-center mt-20 color-effect"><p class="greeting">Hello Person! I'm Blinz</p></div>
+
+<div class="flex items-center justify-center bg-gray-100 mt-5">
+  <div class="w-3/5 p-6 bg-white rounded-lg shadow-lg">
+    <h1 class="text-2xl font-semibold mb-4">Who Is I?</h1>
+    I am Blinz. I am a beginner programmer, I began learning coding stuff about 4 years ago, starting with Scratch then to Web Dev.
+    Now I am mostly focusing on Game Dev, my "dream job" is to become a Game Developer.
+    <br>
+    I LOVE programming.
+  </div>
+</div>
+
+<div class="flex items-center justify-center bg-gray-100 mt-7">
+  <div class="w-3/5 p-6 bg-white rounded-lg shadow-lg">
+    <h1 class="text-2xl font-semibold mb-4">Projects</h1>
+    These are my projects, I usually make games I am currently working on a simple mobile game
+    in Godot and a website used to create timers, stopwatches and much more!
+  </div>
+</div>
+
+<div class="flex items-center justify-center bg-gray-100 mt-7"></div>
+
+<style>
+::selection {
+    background: orange;
+}
+:global(body) {
+    font-family: 'Poppins', sans-serif;
+  }
+</style>
